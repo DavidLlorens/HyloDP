77633ff0c08f450a2d9c7128470ef6b91654af04
David Llorens <dllorens@uji.es>
2020-04-19 11:04:33 +0200

    cleaning

--- runAllExes.sh
+++
@@ -1,4 +0,0 @@
-# Just launch all the executables in the project
-stack build && clear && for p in `cat *.cabal | grep ^executable | sed s'/.* //'g` ; do
-    echo "--------"; echo $p; echo; stack exec $p ;
-done
